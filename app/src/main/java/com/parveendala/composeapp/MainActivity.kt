55b15d062e2e4da9973e3ab959da8d8bf4e4b915
ParveenDala <dparveen94@gmail.com>
2021-11-15 18:00:31 +0530

    Surface with column

--- app/src/main/java/com/parveendala/composeapp/MainActivity.kt
+++ app/src/main/java/com/parveendala/composeapp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.parveendala.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,10 +29,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+private fun MainScreen() {
+    ComposeAppTheme {
+        Surface(
+            color = Color.LightGray,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Surface(
+                color = MaterialTheme.colors.secondary,
+                modifier = Modifier.wrapContentSize(align = Alignment.TopCenter)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Gray),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Greeting("Android")
+                    SubmitButton()
                 }
             }
         }
@@ -41,7 +69,7 @@ fun Greeting(name: String) {
             .padding(16.dp)
             .clickable { println("Text CLicked") },
         style = MaterialTheme.typography.h4,
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
 //        style = TextStyle(
 //            color = Color.DarkGray,
 //            fontSize = 16.sp,
@@ -51,12 +79,21 @@ fun Greeting(name: String) {
     )
 }
 
+@Composable
+fun SubmitButton() {
+    Button(
+        onClick = { println("Button Clicked") },
+        modifier = Modifier
+            .wrapContentSize(align = Alignment.Center)
+            .padding(16.dp)
+    ) {
+        Text(text = "Submit")
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeAppTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
-        }
-    }
+    MainScreen()
 }
