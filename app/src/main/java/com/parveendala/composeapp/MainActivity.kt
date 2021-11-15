9ffde223cea9435ddc7a39e4be7d393319159e96
ParveenDala <dparveen94@gmail.com>
2021-11-15 16:47:22 +0530

    Customising Text Composable

--- app/src/main/java/com/parveendala/composeapp/MainActivity.kt
+++ app/src/main/java/com/parveendala/composeapp/MainActivity.kt
@@ -3,11 +3,18 @@ package com.parveendala.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.parveendala.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,13 +33,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(16.dp)
+            .clickable { println("Text CLicked") },
+        style = MaterialTheme.typography.h4,
+        textAlign = TextAlign.Center
+//        style = TextStyle(
+//            color = Color.DarkGray,
+//            fontSize = 16.sp,
+//            fontFamily = FontFamily.Monospace,
+//            textAlign = TextAlign.Center
+//        )
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+        Surface(color = MaterialTheme.colors.background) {
+            Greeting("Android")
+        }
     }
 }
